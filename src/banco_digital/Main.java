a5be7e7ea020cef87f47d2de6e61cbe67253d4b4
DaianaOdorizzi <d.lealodorizzi@gmail.com>
2024-07-28 15:09:14 -0300

    Banco Digital com Java e Orientação a Objetos

--- src/banco_digital/Main.java
+++ src/banco_digital/Main.java
@@ -3,11 +3,11 @@ package banco_digital;
 public class Main {
 
     public static void main(String[] args) {
-        Cliente venilton = new Cliente();
-        venilton.setNome("Venilton");
+        Cliente daiana = new Cliente();
+        daiana.setNome("Daiana");
 
-        Conta cc = new ContaCorrente(venilton);
-        Conta poupanca = new ContaPoupanca(venilton);
+        Conta cc = new ContaCorrente(daiana);
+        Conta poupanca = new ContaPoupanca(daiana);
 
         cc.depositar(100);
         cc.transferir(100, poupanca);
