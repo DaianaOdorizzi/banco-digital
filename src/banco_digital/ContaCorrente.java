df65d3820f7309d94e07affec2e4fc74aac222ef
DaianaOdorizzi <d.lealodorizzi@gmail.com>
2024-07-28 15:08:01 -0300

    Banco Digital com Java e Orientação a Objetos

--- src/banco_digital/ContaCorrente.java
+++ src/banco_digital/ContaCorrente.java
@@ -1,2 +1,15 @@
-package banco_digital;public class ContaCorrente {
+package banco_digital;
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+    }
+
 }
